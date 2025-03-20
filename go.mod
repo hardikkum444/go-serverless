72e5a722c0af5151f1e07866ac22d0b52dd8be2e
hardikkum444 <hardikkumawat444@gmail.com>
2025-03-20 11:44:56 +0530

    feat: add main file which runs the serverless server

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/hardikkum444/go-serverless
 go 1.24.1
 
 require gopkg.in/yaml.v3 v3.0.1
+
+require github.com/google/uuid v1.6.0 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/hardikkum444/go-serverless/handlers"
+)
 
 func main() {
-	fmt.Println("hello serverless boya")
+	http.HandleFunc("/api/submit", handlers.LoggingMiddleware(handlers.SubmitHandler))
+	// http.HandleFunc("/api/exectute", handlers.LoggingMiddleware(handlers.ExecuteHandler))
+
+	log.Println("serverless system running on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatalf("failed to start server: %v\n", err)
+	}
 }
